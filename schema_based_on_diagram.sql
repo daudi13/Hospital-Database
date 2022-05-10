2f890ca6780547db81a51b7df301063d53225ceb
Abenezer-Tilahun <abenezermesafint2128@gmail.com>
2022-05-10 18:16:11 +0300

    Add a join table for database

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,4 +1,4 @@
-CREATE TABLE patients(
+CREATE TABLE patients (
   id INT GENERATED ALWAYS AS IDENTITY,
   name VARCHAR(255),
   date_of_birth DATE,
@@ -20,3 +20,32 @@ CREATE TABLE patients(
       name VARCHAR(255), 
       PRIMARY KEY(id)
       );
+
+CREATE TABLE medical_histories (
+ id INT GENERATED ALWAYS AS IDENTITY,
+ admitted_at timestamp,
+ patient_id INT,
+ status VARCHAR(100),
+ FOREIGN KEY (patient_id) REFERENCES patients(id),
+ PRIMARY KEY(id)
+);
+
+CREATE TABLE invoice_items (
+id INT GENERATED ALWAYS AS IDENTITY,
+unit_price DECIMAL,
+quantity INT,
+total_price DECIMAL,
+invoice_id INT,
+treatment_id INT,
+FOREIGN KEY (invoice_id) REFERENCES invoices(id),
+FOREIGN KEY (treatment_id) REFERENCES treatments(id),
+PRIMARY KEY (id)
+);
+
+CREATE TABLE medical_histories_treatments (
+medical_history_id INT,
+treatment_id INT,
+FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id),
+FOREIGN KEY (treatment_id) REFERENCES treatments(id),
+PRIMARY KEY (medical_history_id,treatment_id)
+);
